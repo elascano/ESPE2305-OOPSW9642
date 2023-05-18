8b961a4effa74f4bf3dde4712e2681cb65bc1cee
Edison Lascano <edison_lascano@yahoo.com>
2023-05-18 07:10:38 -0500

    folders creation

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+        public static void main(String args[]) {
+                System.out.println("  Hello Object Oriented Programming Students");
+                System.out.println("   ------ My name is Edison Lascano --------");
+                System.out.println("This is going to be a fantastic semester :-) ");
+        }
+}

20537f37fc9aefaae927a28dda951dc792f5d8ec
YeshuaChiliquingaAmaya <yeshuachiliquinga@gmail.com>
2023-05-18 22:44:15 -0500

    add my classwork ChikenFarmSimulator

---
+++ chiliquinga/U1/WS05ChickenFarmSimulator/src/ec/edu/espe/chickenfarm/view/FarmSimulator.java
@@ -0,0 +1,27 @@
+package ec.edu.espe.chickenfarm.view;
+
+import ec.edu.espe.chickenfarm.model.Chicken;
+import ec.edu.espe.chickenfarm.model.Egg;
+import ec.edu.espe.chickenfarm.model.Poop;
+import java.util.Date;
+
+/**
+ *
+ * @author Yeshua Chiliquinga, JSons; DCCO-ESPE
+ */
+public class FarmSimulator {
+    public static void main(String[] args) {
+        Chicken chicken;
+        Egg egg;
+        Poop poop;
+        
+        chicken = new Chicken();
+        chicken.doStuff(0);
+        chicken.setName("Yeshua");
+        chicken.setColor("white and black");
+        chicken.setAge(21);
+        chicken.setIsMolting(true);
+        chicken.setBornOnDate(new Date());
+        System.out.println("this chicken is " + chicken);
+    }
+}

b331b905cb8be7283ac06feb04b88544a5fc2ce1
Edison Verdesoto <edisondamian12@gmail.com>
2023-05-21 20:03:49 -0500

    update folders

---
+++ verdesoto/u1/WSO2HelloWorld/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+public static void main (String args []){
+System.out.println("Hello Classmates");
+System.out.println(" ---- My name is Edison Verdesoto ----- ");
+System.out.println("This is going to be a hard semestre :C");
+}
+}

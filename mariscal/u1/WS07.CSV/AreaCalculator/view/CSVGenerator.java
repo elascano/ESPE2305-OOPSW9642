f64f959477b167320e0f13900455fa70886a3502
Mesias Mariscal <mesiasv2@gmail.com>
2023-05-22 19:44:17 -0500

    Area_V0.2

--- mariscal/u1/WS07.CSV/AreaCalculator/view/CSVGenerator.java
+++ mariscal/u1/WS07.CSV/AreaCalculator/view/CSVGenerator.java
@@ -2,7 +2,6 @@ package ec.edu.espe.areacalculator.view;
 
 import ec.edu.espe.areacalculator.model.FormulaArea;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
@@ -10,12 +9,8 @@ import java.util.List;
 public class CSVGenerator {
     public static void generateCSV(List<FormulaArea> areas, String filename) {
         try {
-            File file = new File(filename);
-            boolean fileExists = file.exists();
-            FileWriter writer = new FileWriter(filename, true);
-            if (!fileExists) {
-                writer.append("Diameter;Area\n");
-            }
+            FileWriter writer = new FileWriter(filename);
+            writer.append("Diameter;Area\n");
             for (FormulaArea area : areas) {
                 float diameter = area.getDiameter();
                 float circleArea = area.calculateCircleArea();
@@ -23,9 +18,9 @@ public class CSVGenerator {
             }
             writer.flush();
             writer.close();
-            System.out.println("Data appended to .csv file: " + filename);
+            System.out.println(".csv generated: " + filename);
         } catch (IOException e) {
-            System.out.println("Error appending data to .csv file: " + e.getMessage());
+            System.out.println("Error generating .csv file: " + e.getMessage());
         }
     }
 }

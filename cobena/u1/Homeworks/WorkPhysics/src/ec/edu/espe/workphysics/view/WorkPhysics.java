e3391dd8ccac00f5062ca3fee85803b412615acf
Joan Cobeña <joancobe64@gmail.com>
2023-05-23 20:54:35 -0500

    Uploading Homework 1 V2.0.0 remaking the formula into one that receive 2 parameters

---
+++ cobena/u1/Homeworks/WorkPhysics/src/ec/edu/espe/workphysics/view/WorkPhysics.java
@@ -0,0 +1,19 @@
+package ec.edu.espe.workphysics.view;
+
+import ec.edu.espe.workphysics.controller.Menu;
+
+/**
+ *
+ * @author Joan Cobeña, KillChain, DCCO-ESPE
+ */
+
+
+
+public class WorkPhysics {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.MainMenu();
+    }
+    
+}

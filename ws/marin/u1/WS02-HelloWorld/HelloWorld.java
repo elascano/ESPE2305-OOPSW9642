864fd6a53a1c0d0f36b4f44349baf873e8ef8249
Marin Alquinga Josue Isaac <132631744+marinjosue@users.noreply.github.com>
2023-05-28 21:55:27 -0500

    Delete

--- ws/marin/u1/WS02-HelloWorld/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        System.out.println("Hello Object Oriented Programming Classmates");
-        System.out.println("----My name is Josue MAarin----");
-        System.out.println("This is going to be a fantastic semester :v");
-
-    }
-}

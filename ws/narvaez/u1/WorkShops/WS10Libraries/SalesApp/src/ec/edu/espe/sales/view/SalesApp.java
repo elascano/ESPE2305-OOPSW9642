db26aaba700d0fd30c57f1ff4111c78122e7c076
LeoNarvaez2503 <leonardo_8.8@live.com>
2023-06-02 23:38:51 -0500

    WS10

---
+++ ws/narvaez/u1/WorkShops/WS10Libraries/SalesApp/src/ec/edu/espe/sales/view/SalesApp.java
@@ -0,0 +1,12 @@
+package ec.edu.espe.sales.view;
+
+/**
+ *
+ * @author Leonardo Narvaez, The FAMSE, DCCO-ESPE
+ */
+public class SalesApp {
+    public static void main(String[] args) {
+        System.out.println("------Leonardo's Sales System-----");
+        System.out.println("What do you want to buy today");
+    } 
+}  

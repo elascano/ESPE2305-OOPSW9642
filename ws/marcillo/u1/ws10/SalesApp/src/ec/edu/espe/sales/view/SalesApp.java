efd2ad41808720b8dc1d670c34d3ed84c7d4530f
JhordyMarcillo <jhordypaulsb2@gmail.com>
2023-06-03 18:52:45 -0500

    update folder

---
+++ ws/marcillo/u1/ws10/SalesApp/src/ec/edu/espe/sales/view/SalesApp.java
@@ -0,0 +1,14 @@
+package ec.edu.espe.sales.view;
+
+/**
+ *
+ * @author USER
+ */
+public class SalesApp {
+
+    public static void main(String[] args) {
+        System.out.println("----Jhordy's Sales System---- ");
+        System.out.println("--What do you want buy today?");
+    }
+    
+}

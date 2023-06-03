a2862cbd6c86a3433fb3c09a4a4e141aee0affef
EspinAndres01 <andresboki2000@gmail.com>
2023-06-03 01:53:48 -0500

    Upload homework and workshop files

---
+++ hw/espin/U1/HW08Json/ChikenFarm/src/espe/edu/ec/controller/ChickenController.java
@@ -0,0 +1,15 @@
+
+package espe.edu.ec.controller;
+
+import espe.edu.ec.view.UserInterface;
+
+/**
+ *
+ * @author Andres Espin, KillChain, DCOO-ESPE
+ */
+public class ChickenController {
+    public static void main(String[] args){
+        UserInterface ui = new UserInterface();
+        
+    }
+}
---
+++ hw/espin/U1/HW08Json/ChikenFarm/src/espe/edu/ec/view/ChickenFarm.java
@@ -0,0 +1,11 @@
+package espe.edu.ec.view;
+
+import espe.edu.ec.model.Chicken;
+import java.util.ArrayList;
+
+public class ChickenFarm {
+    public static void main(String[] args) {
+        UserInterface userInterface = new UserInterface();
+        userInterface.start();
+    }
+}

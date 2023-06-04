1e6363f87556fbf730fdabec3bbaf1afb00ec7d0
Davidmoran9 <davidleonel0408@gmail.com>
2023-06-04 02:06:57 -0500

    Updating Folders

---
+++ ws/moran/U1/WS10LIBRARIES/WS10/SalesApp/src/ec/edu/espe/sales/view/SalesApp.java
@@ -0,0 +1,12 @@
+package ec.edu.espe.sales.view;
+
+/**
+ *
+ * @author Moran David
+ */
+public class SalesApp {
+    public static void main(String[] args) {
+        System.out.println("------David's Sales System-----");
+        System.out.println("What do you want to buy today");
+    } 
+}  

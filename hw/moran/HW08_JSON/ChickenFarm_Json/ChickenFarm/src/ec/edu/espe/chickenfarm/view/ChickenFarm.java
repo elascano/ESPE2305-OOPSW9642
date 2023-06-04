c264e79654d5494cb965e9ef64344aa4e4b8eb3f
Davidmoran9 <davidleonel0408@gmail.com>
2023-06-03 22:30:40 -0500

    updates

---
+++ hw/moran/HW08_JSON/ChickenFarm_Json/ChickenFarm/src/ec/edu/espe/chickenfarm/view/ChickenFarm.java
@@ -0,0 +1,12 @@
+package ec.edu.espe.chickenfarm.view;
+
+/**
+ *
+ * @author Moran David
+ */
+public class ChickenFarm {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.showMenu();
+    }
+}

fda57f80dc0479871ab3bb025d9554cb727d1e70
GuamanJordan <jordanoguaman09@gmail.com>
2023-06-03 21:48:53 -0500

    Create and Upload

---
+++ hw/guaman/U1/ASSIGNMENTS/HW07.CSV.execute/CalculateWithPrinters/CalculatorWithPrinters/src/espe/edu/ec/calculatorwithprinters/view/CalculatorWithPrinters.java
@@ -0,0 +1,19 @@
+package espe.edu.ec.calculatorwithprinters.view;
+import espe.edu.ec.calculatorwithprinters.model.UserOptions;
+
+
+/**
+ * Author: Jordan Guaman, Software Juniors, DCCO-ESPE
+ */
+public class CalculatorWithPrinters {
+
+    public static void main(String[] args) {
+        
+    UserOptions.menu;
+            
+    Presentation.showPresentation(); 
+
+    
+    }
+}
+

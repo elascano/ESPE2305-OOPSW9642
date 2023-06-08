9d9c23325f57ce164d85874fec1acfc488e1f483
Damian Verdesoto <edisondamian12@gmail.com>
2023-06-08 08:05:12 -0500

    try

---
+++ ws/verdesoto/u1/Workshops/WS10SalesApp/SalesApp/src/ec/edu/espe/sales/view/SalesApp.java
@@ -0,0 +1,13 @@
+
+package ec.edu.espe.sales.view;
+
+/**
+ *
+ * @author Edison Verdesoto, Code Warriors, DCCO-ESPE
+ */
+public class SalesApp {
+    public static void main(String[] args) {
+        System.out.println("---- Damian's Sales System ----");
+        System.out.println("What do you want to buy today");
+    }
+}

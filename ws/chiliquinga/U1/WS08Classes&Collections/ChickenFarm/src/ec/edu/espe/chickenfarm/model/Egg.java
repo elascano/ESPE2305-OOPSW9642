dba51d6d115d4a351ae6c07cf2c86d649fa0225f
YeshuaChiliquingaAmaya <yeshuachiliquinga@gmail.com>
2023-06-07 22:47:27 -0500

    uptdate ws

---
+++ ws/chiliquinga/U1/WS08Classes&Collections/ChickenFarm/src/ec/edu/espe/chickenfarm/model/Egg.java
@@ -0,0 +1,28 @@
+package ec.edu.espe.chickenfarm.model;
+
+/**
+ *
+ * @author Yeshua Chiliquinga, JSons; DCCO-ESPE
+ */
+public class Egg {
+    private int id;
+
+    @Override
+    public String toString() {
+        return "Egg{" + "id=" + id + '}';
+    }
+
+    public Egg(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+}

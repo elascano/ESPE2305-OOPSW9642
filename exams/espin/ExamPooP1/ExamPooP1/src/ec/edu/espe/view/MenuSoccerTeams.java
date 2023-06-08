cb3cbed2a34c3cff36dd831ac753a7fada35bf38
EspinAndres01 <andresboki2000@gmail.com>
2023-06-08 08:58:34 -0500

    ExamEspin

---
+++ exams/espin/ExamPooP1/ExamPooP1/src/ec/edu/espe/view/MenuSoccerTeams.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.view;
+
+/**
+ *
+ * @author Andres Espin, KillChain, DCOO-ESPE
+ */
+public class MenuSoccerTeams {
+    SoccerInterface ui=new SoccerInterface();
+    
+    public static void main(String[] args) {
+        SoccerInterface ui=new SoccerInterface();
+        ui.handleSoccerTeam();
+      
+                
+    }
+}

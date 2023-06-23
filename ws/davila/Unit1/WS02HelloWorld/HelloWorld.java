752bccd3d953848a262ab70a758ae5864539416a
Anabel Davila <juanislala2@gmail.com>
2023-06-23 12:02:41 -0500

    Update Ws U1

--- ws/davila/Unit1/WS02HelloWorld/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld{
-	public static void main(String args[]){
-		System.out.println(" hello Classmates ");
-		System.out.println(" ---My names is Pablo Carrera--- ");
-		System.out.println("This is going to be a fantastic semester");
-	}
-}

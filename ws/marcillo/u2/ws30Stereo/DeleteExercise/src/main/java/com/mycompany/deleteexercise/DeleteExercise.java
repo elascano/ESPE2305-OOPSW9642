ff88694bc5e9c7573f769b70ee51618849e9a09d
JhordyMarcillo <jhordypaulsb2@gmail.com>
2023-07-19 00:27:28 -0500

    update folder

---
+++ ws/marcillo/u2/ws30Stereo/DeleteExercise/src/main/java/com/mycompany/deleteexercise/DeleteExercise.java
@@ -0,0 +1,16 @@
+
+
+package com.mycompany.deleteexercise;
+
+import java.util.Scanner;
+/**
+ *
+ * @author USER
+ */
+public class DeleteExercise {
+
+    public static void main(String[] args) {
+
+        
+    }
+}

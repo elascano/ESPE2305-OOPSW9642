3caf299abda1932354e68ea492ffffada0fbe54a
marinjosue <josuisaac2002@gmail.com>
2023-07-20 09:17:09 -0500

    ExamU2

---
+++ exams/marin/u2/theory/Q38-51/Q38-51Marin/src/ec/edu/espe/q38/model/E.java
@@ -0,0 +1,15 @@
+
+package ec.edu.espe.q38.model;
+
+/**
+ *
+ * @author Josue Marin, CodeCrafters: DCCO-ESPE
+ */
+public class E {    
+
+    @Override
+    public String toString() {  
+        return "E{" + '}';
+    }
+    
+}

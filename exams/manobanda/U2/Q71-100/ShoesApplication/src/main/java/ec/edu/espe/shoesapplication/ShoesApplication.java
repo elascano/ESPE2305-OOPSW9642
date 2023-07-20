6ebc59f7a612365483c16da50a59185bab0a7567
Jeffry Manobanda <jnmanobanda@hotmail.com>
2023-07-20 09:03:32 -0500

    exams U2

---
+++ exams/manobanda/U2/Q71-100/ShoesApplication/src/main/java/ec/edu/espe/shoesapplication/ShoesApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espe.shoesapplication;
+
+/**
+ *
+ * @author jnman
+ */
+public class ShoesApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
